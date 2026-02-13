35cb4b8ce37ce30feaefdc38c59d48b8f2b2fcbf
ParinithaK <111687717+ParinithaK@users.noreply.github.com>
2026-02-13 16:23:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import Experience from "./pages/Experience";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/admin/Login";
+import AdminHome from "./pages/admin/Home";
 import Dashboard from "./pages/admin/Dashboard";
 import ArticleForm from "./pages/admin/ArticleForm";
 import ReportsDashboard from "./pages/admin/ReportsDashboard";
@@ -59,7 +60,8 @@ const App = () => (
             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
             {/* Admin Routes */}
             <Route path="/admin/login" element={<Login />} />
-            <Route path="/admin" element={<Dashboard />} />
+            <Route path="/admin" element={<AdminHome />} />
+            <Route path="/admin/pivotal" element={<Dashboard />} />
             <Route path="/admin/reports" element={<ReportsDashboard />} />
             <Route path="/admin/articles/new" element={<ArticleForm />} />
             <Route path="/admin/articles/edit/:id" element={<ArticleForm />} />
